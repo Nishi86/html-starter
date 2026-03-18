6c269695000939a9c37761c5a0245827457d564c
Nishi86 <100294793+Nishi86@users.noreply.github.com>
2026-03-18 12:33:00 +0530

    Update cleverpush-worker.js

--- cleverpush-worker.js
+++ cleverpush-worker.js
@@ -1,1 +1,1 @@
-importScripts('https://static.cleverpush.com/channel/worker/zsKod4ggm8r8XzCev.js' + self.location.search);
+importScripts('https://static.cleverpush.com/channel/worker/5sYSxtSQpom2sAsnX.js' + self.location.search);
